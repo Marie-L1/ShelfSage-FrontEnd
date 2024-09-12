b0a39a60dca767ad062a5008159ec642e62501cb
Maria Lukowich <mlukowich27@gmail.com>
2024-09-12 11:14:00 -0600

    sass variables, mixins, etc. Starting the header

--- ShelfSage/src/App.jsx
+++ ShelfSage/src/App.jsx
@@ -6,6 +6,33 @@ function App() {
 
   return (
     <>
+    <header>
+      <img className="" src="./assets/images/SHELFSAGE.png" alt="shelfsage logo"></img>
+      <div>
+        <img src="#"></img>
+        <h2></h2>
+        <></>
+      </div>
+      <ul className="nav">
+        <li className='nav__home'>
+          <img src="./assets/icons/home.svg"></img>
+          <h3>Home</h3>
+        </li>
+        <li className="nav__shelf">
+          <img src="./assets/icons/shelf.svg"></img>
+          <h3>Shelf</h3>
+        </li>
+        <li className="nav__rec">
+          <img src="./assets/icons/recommendations.svg"></img>
+          <h3>Recommendations</h3>
+        </li>
+        <li className="nav__profile">
+          <img src="./assets/icons/profile.svg"></img>
+          <h3>Profile</h3>
+        </li>
+      </ul>
+    </header>
+    
 
     </>
   )
