8bbe89fcfc24c72fb0bdaae46872fd227fafa9a7
Maria Lukowich <mlukowich27@gmail.com>
2024-09-14 16:43:17 -0600

    making the page files and basic routing

--- ShelfSage/src/App.jsx
+++ ShelfSage/src/App.jsx
@@ -2,20 +2,23 @@ import "./App.scss";
 import React from "react";
 import { BroswerRouter, Router, Route, Switch, Redirect } from "react-router-dom";
 import { useAuth } from "./script/AuthContent"
-import Header from "./components/Header/Header";
+import Header from "./components/Header/Header.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Shelf from "./pages/Shelf/Shelf.jsx";
+import Recommendations from "./pages/Recommendations/Recommendations.jsx";
+import Profile from "./pages/Profile/Profile.jsx";
 
 function App() {
   const { user, loading } = useAuth();
   
   return (
     <>
+    <BroswerRouter>
     <Header />
-    <section className="main-content">
-      
+    <Router>
 
-    </section>
-    
-
+    </Router>
+    </BroswerRouter>
     </>
   )
 }
---
+++ ShelfSage/src/pages/Recommendations/Recommendations.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Recommendations.scss";
+
+function Recommendations() {
+  return (
+    <div>Recommendations</div>
+  )
+}
+
+export default Recommendations;
