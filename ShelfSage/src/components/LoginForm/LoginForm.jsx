aea59937d107e127e0b9fd13a7e0f5075e7ce7b3
Maria Lukowich <mlukowich27@gmail.com>
2024-09-16 16:27:55 -0600

    creating the forms

--- ShelfSage/src/components/LoginForm/LoginForm.jsx
+++ ShelfSage/src/components/LoginForm/LoginForm.jsx
@@ -1,25 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function LoginForm({ onSubmitForm }) {
+function LoginForm({ onSubmitForm, onSwitch }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userInfo = { username, password };
+    console.log("login submitted");
+
+    onSubmitForm(userInfo);
+
+    // clear the input fields 
+    setUsername("");
+    setPassword("");
+
+    // redirect to logged-in homepage
+    navigate("/");
+  }
 
 
   return (
-    <div>
-        <h2 className="">Login</h2>
-        <form className="">
-            <div className="">
-                <label className="">Username</label>
-                <input className="" placeholder="username"></input>
+    <div className="login">
+        <h2 className="login__title">Login</h2>
+        <form className="login__form" onSubmit={handleSubmit}>
+            <div className="login__username-wrapper">
+                <label className="login__username-label">Username</label>
+                <input
+                  name="username"
+                  className="login__username"
+                  placeholder="username"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                  required
+                />
             </div>
 
-            <div className="">
-                <label className="">Password</label>
-                <input className="" placeholder="password"></input>
+            <div className="login__password-wrapper">
+                <label className="login__password-label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="login__password"
+                  placeholder="password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  required
+                />
             </div>
 
-            <button className="" type="submit">Login</button>
+            <button className="login__submit-btn" type="submit">Login</button>
 
-            <p className="">Don't have an account? <button className="" type="button"  onClick={() => onSwitch("signup")}>Signup</button></p>
+            <p className="login__signup-btn">Don't have an account? 
+              <button className="" type="button" onClick={() => onSwitch("signup")}>
+              Signup
+            </button>
+            </p>
         </form>
     </div>
   )
--- ShelfSage/src/components/SignupForm/SignupForm.jsx
+++ ShelfSage/src/components/SignupForm/SignupForm.jsx
@@ -1,30 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function SignupForm() {
+
+function SignupForm({ onSwitch }) {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // prevent the form from submitting and reloading the signup page
+
+        const newUser = { username, email, password };
+        console.log("form submitted")
+
+        // update user state to log them in
+        setUser(newUser);
+
+        // clear input fields 
+        setUsername("");
+        setEmail("");
+        setPassword("");
+
+        // redirect to logged-in homepage
+        navigate("/")
+    } 
+
   return (
-    <div>
-    <h2 className="">Signup</h2>
-    <form className="">
-        <div className="">
-            <label className="">Username</label>
-            <input className="" placeholder="username"></input>
+    <section className="signup">
+    <h2 className="signup__title">Signup</h2>
+    <form className="signup__forms" onSubmit={handleSubmit}>
+        <div className="signup__username-wrapper">
+            <label className="signup__username-label">Username</label>
+            <input
+            name="username"
+            className="signup__username-input"
+            placeholder="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
         </div>
 
-        <div className="">
-            <label className="">Email</label>
-            <input className="" placeholder="email"></input>
+        <div className="signup__email-wrapper">
+            <label className="signup__email-label">Email</label>
+            <input
+            name="email"
+            className="signup__email-input"
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+
         </div>
 
-        <div className="">
-            <label className="">Password</label>
-            <input className="" placeholder="password"></input>
+        <div className="signup__password-wrapper">
+            <label className="signup__password-label">Password</label>
+            <input
+            name="password"
+            className="signup__password-input"
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
         </div>
 
-        <button className="" type="submit">Login</button>
+        <button className="signup__submit-btn" type="submit">Sign Up</button>
 
-        <p className="">Already have an account? <button className="" type="button"  onClick={() => onSwitch("login")}>Signup</button></p>
+        <p className="signup__switch">
+            Already have an account? <button type="button" onClick={() => onSwitch("login")}>Login</button>
+        </p>
     </form>
-</div>
+</section>
   )
 }
 
