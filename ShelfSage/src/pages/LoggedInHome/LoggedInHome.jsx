8803916d1b763d18dd2f15c8bcce5dd54cd56066
Maria Lukowich <mlukowich27@gmail.com>
2024-09-17 15:27:43 -0600

    starting to style the logged in homepage

--- ShelfSage/src/pages/LoggedInHome/LoggedInHome.jsx
+++ ShelfSage/src/pages/LoggedInHome/LoggedInHome.jsx
@@ -2,19 +2,21 @@ import React, {useState} from "react";
 import "./LoggedInHome.scss";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import backgroundImage from '../../assets/images/pages-img.jpg';
 
 function Home() {
   return (
     <section className="home">
       <div className="hero">
-        <h2></h2>
-        <SearchBar classname="hero__search" />
+        <img className="hero__img" src={backgroundImage} alt="open books"></img>
+        <div className="hero__content-wrapper">
+          <h2 className="hero__title">Find Your Books</h2>
+          <SearchBar classname="hero__search" />
+        </div>
       </div>
       <div className="popular-books">
         <h2>Popular Books</h2>
-        <ul>
-
-        </ul>
+        
       </div>
     </section>
   )
