deff30d4c6c27bdacb8bee9193870d16900ef317
Maria Lukowich <mlukowich27@gmail.com>
2024-09-18 11:31:24 -0600

    setting up the basics for each page

--- ShelfSage/src/pages/Shelf/Shelf.jsx
+++ ShelfSage/src/pages/Shelf/Shelf.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Shelf.scss";
+import { useAuth } from "../../script/AuthContent.jsx";
+import backgroundImage from "../../assets/images/shelf-img.jpg"
 
 function Shelf() {
+  const { user } = useAuth();
+
   return (
-    <div>Shelf</div>
+    <section className="home">
+    <div className="hero">
+      <img className="hero__img" src={backgroundImage} alt="open books"></img>
+      <div className="hero__content-wrapper">
+        <h2 className="hero__title">{user.username}'s Books</h2>
+      </div>
+    </div>
+    <div className="user-books">
+
+    </div>
+  </section>
   )
 }
 
